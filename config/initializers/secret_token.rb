63c644c4709a30f8762e6fd925a783fc05e78691
ravi <rsravishri30@gmail.com>
2014-05-25 11:10:02 +0530

    new rails 4 depot app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot4r4::Application.config.secret_key_base = 'b8207a49565e66166746b1cb9eba5ac7e0fa5ff6ada43dc62afdfb65f51b85d17c3a01323b53b581db09f0d4c0045379c5aa336ff9427adb704cce97cad0cadd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Depot4r4::Application.config.session_store :cookie_store, key: '_depot4r4_session'
